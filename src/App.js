0e2e5dc03f7ce5ae85d496b3677db7d46eadc3d3
Roxanne Reyes <34749680+waetech@users.noreply.github.com>
2020-04-06 22:34:41 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+
+import { CardList } from './components/card-list/card-list.component';
+
+import { SearchBox } from './components/search-box/search-box.component';
+
+
+
+class App extends Component{
+  constructor() {
+    super();
+    this.state = {
+      monsters: [],
+      searchField: ''
+    };
+    
+  }
+
+  
+
+  handleChange = (e) => {
+    this.setState({searchField: e.target.value})
+  }
+
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({ monsters: users }));
+  }
+
+
+  render(){
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster => 
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+      );
+
+     return(
+      <div className='App'>
+        <h1>Monster Rolodex</h1>
+          <SearchBox 
+            placeholder = 'Search monsters'
+            handleChange = {this.handleChange}
+        />
+        <CardList monsters={filteredMonsters} />
+       
+     </div>
+
+    );
+  }
+}
+
+
+export default App;
---
+++ src/components/search-box/search-box.component.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './search-box.styles.css';
+
+export const SearchBox = ({ placeholder, handleChange }) => (
+    <div>
+         <input 
+         className='search' 
+         type='search' 
+         placeholder= {placeholder}
+         onChange= {handleChange}
+          />
+    </div>
+)
